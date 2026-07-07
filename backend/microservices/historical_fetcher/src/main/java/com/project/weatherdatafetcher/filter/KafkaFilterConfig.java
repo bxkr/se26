01d6e7c846263dc11b3195d1cea2a0744ecd541b
ShadowSoldier1231 <blacklight1231@gmail.com>
2026-07-07 21:58:42 +0300

    bug fixes

--- backend/microservices/historical_fetcher/src/main/java/com/project/weatherdatafetcher/filter/KafkaFilterConfig.java
+++ backend/microservices/historical_fetcher/src/main/java/com/project/weatherdatafetcher/filter/KafkaFilterConfig.java
@@ -2,18 +2,35 @@ package com.project.weatherdatafetcher.filter;
 
 
 import com.project.weatherdatafetcher.dto.InputEvent;
-import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.jspecify.annotations.NonNull;
+
+import org.apache.commons.logging.LogFactory;
+import org.jspecify.annotations.NullMarked;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.listener.adapter.RecordFilterStrategy;
+import org.springframework.kafka.support.KafkaUtils;
+import org.springframework.kafka.support.serializer.SerializationUtils;
+import org.springframework.core.log.LogAccessor;
 
 @Configuration
+@NullMarked
 public class KafkaFilterConfig {
 
+    private static final LogAccessor logger = new LogAccessor(LogFactory.getLog(KafkaFilterConfig.class));
+
     @Bean
-    public RecordFilterStrategy<Object, Object> datasetTypeFilter() {
+    public RecordFilterStrategy<String, Object> datasetTypeFilter() {
         return consumerRecord -> {
+
+            System.out.println("Event is being filtered");
+            if (SerializationUtils.getExceptionFromHeader(
+                    consumerRecord,
+                    KafkaUtils.VALUE_DESERIALIZER_EXCEPTION_HEADER,
+                    logger) != null) {
+
+                return false;
+            }
+
             Object value = consumerRecord.value();
 
 
@@ -21,10 +38,11 @@ public class KafkaFilterConfig {
                 if (event.dataset_type() == null) {
                     return true;
                 }
+
                 return !event.dataset_type().equals("weather-raw");
             }
 
-            return true;
+            return false;
         };
     }
 }
